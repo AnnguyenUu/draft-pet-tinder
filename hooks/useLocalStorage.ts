660e0c148f1558f8ea42f26e8e5cd9c39001973c
AnnguyenUu <annguyenthanh292@gmail.com>
2026-07-17 00:25:46 +0700

    test eslint

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
+  const a = true;
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key);
